2ce55bb1bc7f56d7aaae914d58912948c9d93a2d
abraham.menyhart <abraham.menyhart@dpc.hu>
2023-05-22 17:21:53 +0200

    PS-1400: Relation not exists exception handling added

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/core/service/database/PostgreSQLQueryService.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/core/service/database/PostgreSQLQueryService.java
@@ -18,19 +18,25 @@
  */
 package org.apache.fineract.infrastructure.core.service.database;
 
-import static java.lang.String.format;
-
-import java.util.List;
-import java.util.Objects;
-import javax.sql.DataSource;
+import org.apache.fineract.infrastructure.dataqueries.exception.DatatableNotFoundException;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+
+import static java.lang.String.format;
+import static java.util.Objects.nonNull;
+import static org.apache.fineract.infrastructure.core.service.database.PostgresSQLExceptionCode.RELATION_NOT_EXISTS;
+
 @Component
 public class PostgreSQLQueryService implements DatabaseQueryService {
-
     private final DatabaseTypeResolver databaseTypeResolver;
 
     @Autowired
@@ -56,12 +62,32 @@ public class PostgreSQLQueryService implements DatabaseQueryService {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         String sql = "SELECT attname AS COLUMN_NAME, not attnotnull AS IS_NULLABLE, atttypid::regtype  AS DATATYPE, attlen AS CHARACTER_MAXIMUM_LENGTH, attnum = 1 AS COLUMN_KEY FROM pg_attribute WHERE attrelid = '\""
                 + tableName + "\"'::regclass AND attnum > 0 AND NOT attisdropped ORDER BY attnum";
-        final SqlRowSet columnDefinitions = jdbcTemplate.queryForRowSet(sql); // NOSONAR
+        final SqlRowSet columnDefinitions = query(jdbcTemplate, sql, tableName); // NOSONAR
         if (columnDefinitions.next()) {
             return columnDefinitions;
         } else {
             throw new IllegalArgumentException("Table " + tableName + " is not found");
         }
+    }
+
+    @NotNull
+    private static SqlRowSet query(JdbcTemplate jdbcTemplate, String sql, String tableName) {
+        try {
+            return jdbcTemplate.queryForRowSet(sql);
+        } catch (DataAccessException e) {
+            if (isRelationNotExistsException(e)) {
+                throw new DatatableNotFoundException(tableName);
+            } else {
+                throw e;
+            }
+        }
+    }
+
+    private static boolean isRelationNotExistsException(DataAccessException e) {
+        if (e.getCause() instanceof SQLException sqlException) {
+            return nonNull(sqlException.getSQLState()) && sqlException.getSQLState().equals(RELATION_NOT_EXISTS);
+        }
+        return false;
     }
 
     @Override
